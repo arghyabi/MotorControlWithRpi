b921cca771506ad72680d2398faba1d74b6af87b
Arghya Biswas <arghyabiswas05@gmail.com>
2025-09-22 22:10:07 +0530

    Added the valve settings (#5)
    Signed-off-by: Arghya Biswas <arghyabiswas05@gmail.com>

--- Scripts/Common.py
+++ Scripts/Common.py
@@ -22,10 +22,21 @@ SCRIPT_FOLDER           = "Scripts"
 CONFIG_UPDATE_AVAILABLE = True
 NO_CONFIG_UPDATE        = False
 
-TANK_HEIGHT             = 60  # Example tank height
-TOP_EMPTY_DISTANCE      = 5
-BOTTOM_FULL_DISTANCE    = 5
-ONE_THIRD_LEVEL         = TANK_HEIGHT / 3
+TANK_HEIGHT             = 125  # Example tank height
+TOP_EMPTY_DISTANCE      = 25
+BOTTOM_FULL_DISTANCE    = 65
+ONE_THIRD_LEVEL         = TANK_HEIGHT // 3
 
 CHECK_INTERVAL_SECONDS  = 1
 MAX_PRE_NIGHT_FILL_TIME = 600
+
+MORNING_7AM             = 7
+MORNING_8AM             = 8
+EVENING_7PM             = 19
+EVENING_8PM             = 20
+NIGHT_10PM              = 22
+NIGHT_9PM               = 21
+
+
+VALVE1_DEFAULT_DURATION = 1  # in minutes
+VALVE2_DEFAULT_DURATION = 1  # in minutes
