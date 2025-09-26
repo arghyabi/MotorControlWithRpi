6bc5b86d651652453253fde2be4fab4ee2179ef6
Arghya Biswas <arghyabiswas05@gmail.com>
2025-09-26 10:45:27 +0530

    Fix frequent motor on off issue
    Signed-off-by: Arghya Biswas <arghyabiswas05@gmail.com>

--- Scripts/Common.py
+++ Scripts/Common.py
@@ -22,21 +22,58 @@ SCRIPT_FOLDER           = "Scripts"
 CONFIG_UPDATE_AVAILABLE = True
 NO_CONFIG_UPDATE        = False
 
+# ------[_]--------      <------------|
+# |               |         25        |
+# |...............|      <------      |
+# |               |            |      |
+# |               |            |      |
+# |               |            |      |
+# |               |            |     125
+# |               |            |      |
+# |               |           70      |
+# |               |            |      |
+# |               |            |      |
+# |               |            |      |
+# |...............|      <------      |
+# |               |          |        |
+# |               |         30        |
+# |               |          |        |
+# -----------------      <------------|
+
 TANK_HEIGHT             = 125  # Example tank height
 TOP_EMPTY_DISTANCE      = 25
-BOTTOM_FULL_DISTANCE    = 65
-ONE_THIRD_LEVEL         = TANK_HEIGHT // 3
+BOTTOM_FULL_DISTANCE    = 30
+
+MAX_WATER_LEVEL         = TANK_HEIGHT - TOP_EMPTY_DISTANCE
+TWO_THIRD_LEVEL         = MAX_WATER_LEVEL // 3 * 2  # Two-thirds full level
 
 CHECK_INTERVAL_SECONDS  = 1
 MAX_PRE_NIGHT_FILL_TIME = 600
 
+NIGHT_12AM              = 0
+NIGHT_1AM               = 1
+NIGHT_2AM               = 2
+NIGHT_3AM               = 3
+NIGHT_4AM               = 4
+NIGHT_5AM               = 5
+MORNING_6AM             = 6
 MORNING_7AM             = 7
 MORNING_8AM             = 8
+MORNING_9AM             = 9
+MORNING_10AM            = 10
+MORNING_11AM            = 11
+NOON_12PM               = 12
+AFTERNOON_1PM           = 13
+AFTERNOON_2PM           = 14
+AFTERNOON_3PM           = 15
+AFTERNOON_4PM           = 16
 AFTERNOON_5PM           = 17
+EVENING_6PM             = 18
 EVENING_7PM             = 19
 EVENING_8PM             = 20
-NIGHT_10PM              = 22
 NIGHT_9PM               = 21
+NIGHT_10PM              = 22
+NIGHT_11PM              = 23
 
 
 VALVE1_DEFAULT_DURATION = 1  # in minutes
--- Scripts/Utility.py
+++ Scripts/Utility.py
@@ -1,6 +1,6 @@
 import json
 import os
-from Common import RT_DB_FILE
+from Common import *
 
 def readRtDb():
     try:
@@ -23,3 +23,37 @@ def writeRtDb(motorStatus=None, tankLevel=None, configUpdateAvailable=None):
             json.dump(db, f)
     except Exception as e:
         print(f"Error writing to {RT_DB_FILE}: {e}")
+
+
+def ifWaterLevelBelowTwoThird(waterLevel):
+    # Allow a margin of +/- 5 cm around TWO_THIRD_LEVEL for consistency (hysteresis)
+    margin = 5
+    if waterLevel < (TWO_THIRD_LEVEL - margin):
+        return True
+    elif waterLevel > (TWO_THIRD_LEVEL + margin):
+        return False
+    else:
+        return False
+
+
+def ifWaterLevelAboveMax(waterLevel):
+    # Allow a margin of +/- 5 cm around MAX_WATER_LEVEL for consistency (hysteresis)
+    margin = 5
+    if waterLevel > (MAX_WATER_LEVEL + margin):
+        return True
+    elif waterLevel < (MAX_WATER_LEVEL - margin):
+        return False
+    else:
+        return False
+
+
+def ifWaterLevelBelowMin(waterLevel):
+    # Allow a margin of +/- 5 cm around BOTTOM_FULL_DISTANCE for consistency (hysteresis)
+    margin = 5
+    minLevel = TANK_HEIGHT - BOTTOM_FULL_DISTANCE
+    if waterLevel < (minLevel - margin):
+        return True
+    elif waterLevel > (minLevel + margin):
+        return False
+    else:
+        return False
